50cf4145b1cbb091705c4970e306cf1b8d884eb4
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-04-29 19:23:30 -0600

    Ejercicio: 3.5: Backend de la Agenda Telefónica, paso 5 - COMPLETADO

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
     id: 1,
@@ -57,6 +59,20 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
 
     responde.status(204).end()
+})
+
+app.post('/api/persons/', (request, response) => {
+  const body = request.body
+
+  const newPerson = {
+    id: Math.floor(Math.random() * 1000000),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(newPerson)
+
+  response.json(newPerson)
 })
 
 const PORT = 3001
